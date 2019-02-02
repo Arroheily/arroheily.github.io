05f82f87f4f7e823c2c96a54d3dbb612e46ad91e
Hui <hui@Arroheily-Huis-MacBook-Pro-2.local>
2019-02-02 18:57:57 +0800

    新的react项目的初始化

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
